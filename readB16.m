80d652c7977c0cb3d37813646459dca36401d2a3
lucapiombo <luca.piomboni@mail.polimi.it>
2025-06-05 19:20:26 +0200

    Improved code documentation

--- readB16.m
+++ readB16.m
@@ -1,33 +1,4 @@
-function image = readB16(dirPath, filename)
-% readB16.m  Read PCO *.b16 image file
-%
-% image = readB16(filename)
-%       = readB16(dirPath, filename)
-%
-% where: image - 2D array of image
-%     filename - path and filename (string)
-%      dirPath - optional directory path (string)
-% 
-% Example usage: 
-%  For a file 'image.b16' located in the local directory 'files', the following
-%  will be successful
-%    image = readB16('files/image.b16')
-%    image = readB16('files/','image.b16')
-%
-%  and the following will cause failure with fd=-1
-%    image = readB16('files/image.B16')
-%    image = readB16('files','image.b16')
-%    image = readB16('files','image.B16')
-% 
-% Image is returned as a 2D array, where only 1 image is stored in each *.b16 file.
-%
-% Only b/w images have been tested, and importing color images should 
-% throw an error.
-%
-% Author: Carl Hall
-% Date: Spring 2016
-% Modification History
-%  March 2016 - Original Code
+function image = readB16(filePath)
 %% b16 Header byte Structure
 % 
 % Bytes    Type      Description
@@ -53,17 +24,10 @@ function image = readB16(dirPath, filename)
 % ?        chars     Comment file in ASCII characters with variable length of 0...XX. 
 %                              The length of the comment filed must be documented in the �header length� field. 
 %          uint16    16 bit pixel data, starting at offset given by the 'header size' int32
-%% Start of Code
-% Parse optional input
-if nargin>1
-    filename = fullfile(dirPath,filename);
-else
-    filename = dirPath;
-end
 % Open the file
-fd = fopen(filename,'r');
+fd = fopen(filePath,'r');
 if(fd < 0)
-  error('Could not open file: %s',filename)
+  error('Could not open file: %s',filePath)
 end
 % Check that it is a PCO file
 filetype = fread(fd, 4, 'char');
